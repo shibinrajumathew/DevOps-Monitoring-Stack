6831e0aceda4dfb3ec9ad770f2905be22d690ba7
Shibin Raju Mathew <shibinrajumathew@vveeo.com>
2025-04-02 14:30:02 +0100

    metric data format - add host status

--- process_to_prometheus.sh
+++ process_to_prometheus.sh
@@ -12,6 +12,33 @@ while true; do
         }' /opt/nagios/var/host-perfdata > $OUTFILE
     fi
     
+    # Add Host UP/DOWN status to metrics from status.dat
+    if [ -f "/opt/nagios/var/status.dat" ]; then
+        awk '{
+            if ($1 == "hoststatus") {
+            # Extract the host name and current state from the hoststatus block
+            host_name=""; state="";
+            
+            # Parse the host_name and current_state
+            for (i = 1; i <= NF; i++) {
+                if ($i == "host_name=") {
+                    host_name=$(i+1);
+                }
+                if ($i == "current_state=") {
+                    state=$(i+1);
+                }
+            }
+
+            # Check current state: 0 means UP, 1 means DOWN
+            if (state == "0") {
+                print "nagios_host_up{host=\""host_name"\"} 1";  # UP status
+            } else {
+                print "nagios_host_up{host=\""host_name"\"} 0";  # DOWN status
+            }
+        }
+    }' /opt/nagios/var/status.dat >> $OUTFILE
+    fi
+    
     # Process service metrics
     if [ -f "/opt/nagios/var/service-perfdata" ]; then
         awk -F'\t' '{
