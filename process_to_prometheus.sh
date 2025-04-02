3f885232536b58527621fdb25a3c50d441c422a4
Shibin Raju Mathew <shibinrajumathew@vveeo.com>
2025-04-02 16:49:02 +0100

    host state added

--- process_to_prometheus.sh
+++ process_to_prometheus.sh
@@ -7,8 +7,8 @@ while true; do
         awk -F'\t' '{
             split($4, metrics, ";");
             gsub(/.*=/, "", metrics[1]);
-            print "nagios_host_rta_seconds{host=\""$3"\"}", metrics[1]/1000;
-            print "nagios_host_pl{host=\""$3"\"}", metrics[5]
+            print "nagios_host_rta_seconds{host=\""$3"\",hoststate=\""$5"\"}", metrics[1]/1000;
+            print "nagios_host_pl{host=\""$3"\", hoststate=\""$5"\"}", metrics[5]
         }' /opt/nagios/var/host-perfdata > $OUTFILE
     fi
     
@@ -45,11 +45,11 @@ while true; do
             split($5, metrics, ";");
             if ($3 == "HTTP") {
                 gsub(/.*=/, "", metrics[1]);
-                print "nagios_http_response_seconds{host=\""$2"\"}", metrics[1]/1000 >> $OUTFILE
+                print "nagios_http_response_seconds{host=\""$2"\", hoststate=\""$6"\"}", metrics[1]/1000 >> $OUTFILE
             }
             if ($3 == "PING") {
                 gsub(/.*=/, "", metrics[1]);
-                print "nagios_ping_rta_seconds{host=\""$2"\"}", metrics[1]/1000 >> $OUTFILE
+                print "nagios_ping_rta_seconds{host=\""$2"\", hoststate=\""$6"\"}", metrics[1]/1000 >> $OUTFILE
             }
         }' /opt/nagios/var/service-perfdata >> $OUTFILE
     fi
