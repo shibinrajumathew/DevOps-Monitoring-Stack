54976e8436a391ccfa6738c5fb14af5da0b06f0f
Shibin Raju Mathew <shibinrajumathew@vveeo.com>
2025-04-05 23:10:06 +0100

    service perf format update

--- nagios-config/perf-processor/process_service_perfdata.sh
+++ nagios-config/perf-processor/process_service_perfdata.sh
@@ -4,14 +4,14 @@ NAGIOS_SERVICE_PERFDATA="/opt/nagios/var/service-perfdata"
 
 if [ -f "$NAGIOS_SERVICE_PERFDATA" ]; then
     awk -F'\t' '{
-        split($5, metrics, ";");
-        if ($3 == "HTTP") {
-            gsub(/.*=/, "", metrics[1]);
-            print "nagios_http_response_seconds{host=\""$2"\",hoststate=\""$6"\"} " metrics[1]/1000;
-        }
-        if ($3 == "PING") {
-            gsub(/.*=/, "", metrics[1]);
-            print "nagios_ping_rta_seconds{host=\""$2"\",hoststate=\""$6"\"} " metrics[1]/1000;
-        }
-    }' "$NAGIOS_SERVICE_PERFDATA" > "$OUTFILE"
+            split($5, metrics, ";");
+            if ($3 == "HTTP") {
+                gsub(/.*=/, "", metrics[1]);
+                print "nagios_http_response_seconds{host=\""$3"\", hoststate=\""$7"\"}", metrics[1]/1000;
+            }
+            if ($3 == "PING") {
+                gsub(/.*=/, "", metrics[1]);
+                print "nagios_ping_rta_seconds{host=\""$3"\", hoststate=\""$7"\"}", metrics[1]/1000;
+            }
+        }' "$NAGIOS_SERVICE_PERFDATA" > "$OUTFILE"
 fi
