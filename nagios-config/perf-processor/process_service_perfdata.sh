97692133dda555a26eda5fd0378d95b69fdd8ae8
Shibin Raju Mathew <shibinrajumathew@vveeo.com>
2025-04-05 23:39:37 +0100

    service .sh fix

--- nagios-config/perf-processor/process_service_perfdata.sh
+++ nagios-config/perf-processor/process_service_perfdata.sh
@@ -4,14 +4,41 @@ NAGIOS_SERVICE_PERFDATA="/opt/nagios/var/service-perfdata"
 
 if [ -f "$NAGIOS_SERVICE_PERFDATA" ]; then
     awk -F'\t' '{
-            split($5, metrics, ";");
-            if ($3 == "HTTP") {
-                gsub(/.*=/, "", metrics[1]);
-                print "nagios_http_response_seconds{host=\""$3"\", hoststate=\""$7"\"}", metrics[1]/1000;
+        # Extract fields from the template
+        hostname = "";
+        service = "";
+        perfdata = "";
+        for (i=1; i<=NF; i++) {
+            if ($i ~ /^HOSTNAME::/) split($i, h, "::");
+            if ($i ~ /^SERVICEDESC::/) split($i, s, "::");
+            if ($i ~ /^SERVICEPERFDATA::/) split($i, p, "::");
+        }
+        hostname = h[3];
+        service = s[3];
+        perfdata = p[3];
+        
+        # Process performance data
+        if (service == "HTTP") {
+            split(perfdata, metrics, ";");
+            for (m in metrics) {
+                if (metrics[m] ~ /=/) {
+                    split(metrics[m], kv, "=");
+                    if (kv[1] ~ /time/) {
+                        print "nagios_http_response_seconds{host=\""hostname"\"} " kv[2]/1000;
+                    }
+                }
             }
-            if ($3 == "PING") {
-                gsub(/.*=/, "", metrics[1]);
-                print "nagios_ping_rta_seconds{host=\""$3"\", hoststate=\""$7"\"}", metrics[1]/1000;
+        }
+        else if (service == "PING") {
+            split(perfdata, metrics, ";");
+            for (m in metrics) {
+                if (metrics[m] ~ /=/) {
+                    split(metrics[m], kv, "=");
+                    if (kv[1] ~ /rta/) {
+                        print "nagios_ping_rta_seconds{host=\""hostname"\"} " kv[2]/1000;
+                    }
+                }
             }
-        }' "$NAGIOS_SERVICE_PERFDATA" > "$OUTFILE"
+        }
+    }' "$NAGIOS_SERVICE_PERFDATA" > "$OUTFILE"
 fi
